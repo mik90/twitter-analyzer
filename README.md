b50d139906108ae0303f7e7d53c21076bde478bd
Mike Kaliman <kaliman.mike@gmail.com>
2020-09-23 22:12:12 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # twitter-bot
+
+## conf
+### accounts.json
+Stores accounts whose replies will be analyzed, they can also be the start of a search for
+networks of accounts.
+
+### auth
+Twitter API keys and tokens
+
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+
+// TODO Function that reads in keys/tokens
+// bearer token might do, don't forget to strip the newlines
+
+// TODO function that looks through accounts.json
+
+struct Account {
+    handle: String, // Includes "@"
+    category: String,
+}
+
+fn main() {
+
+    // Well, i gotta redo all the stuffs
+    println!("Hello, world!");
+}
